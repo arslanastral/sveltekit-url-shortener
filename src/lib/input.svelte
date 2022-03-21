38d6ac179c33598b9b0804ab23e16bf291ffeb74
Arslan <arslanastral@outlook.com>
2022-03-21 19:38:46 +0800

    added input component

---
+++ src/lib/input.svelte
@@ -0,0 +1,11 @@
+<div class="input-container" />
+
+<style>
+  .input-container {
+    width: 800px;
+    height: 60px;
+    background: #ffffff;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    border-radius: 42px;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,17 @@
 <script>
   import Hero from '$lib/hero.svelte';
+  import Input from '$lib/input.svelte';
 </script>
 
 <div class="content">
   <Hero />
+  <Input />
 </div>
 
 <style>
   .content {
     display: flex;
-    justify-content: center;
+    align-items: center;
+    flex-direction: column;
   }
 </style>
