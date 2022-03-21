c2efe0c1fa05d9c3568133dfc6469e7516f02032
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-21 19:56:07 +0800

    button styling

--- src/lib/input.svelte
+++ src/lib/input.svelte
@@ -2,7 +2,7 @@
   import Button from '$lib/button.svelte';
 </script>
 
-<div class="input-container">
+<div class="input-container flex">
   <Button
     title="Shortern"
     --font-size="20px"
@@ -10,10 +10,16 @@
     --color="white"
     --bg-color="#3E5DFF"
     --border-radius="29px"
+    --margin="5px"
   />
 </div>
 
 <style>
+  .flex {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+
   .input-container {
     width: 800px;
     height: 60px;
