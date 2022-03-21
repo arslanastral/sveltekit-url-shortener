152afac87ad490037dc674974ed7131709dc3ca9
Arslan <arslanastral@outlook.com>
2022-03-21 14:02:13 +0800

    init 03_URL-Shortener-Microservice

---
+++ src/routes/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import 'sanitize.css';
+  import 'sanitize.css/forms.css';
+  import 'sanitize.css/typography.css';
+  import 'animate.css';
+</script>
+
+<h1>URL Shortener Microservice</h1>
+<p>Project Init</p>
