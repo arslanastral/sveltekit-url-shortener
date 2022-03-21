fa8a36e367673775a1f2bfb1af842e2fce733c07
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-21 15:56:22 +0800

    added hero message

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,5 +5,15 @@
   import 'animate.css';
 </script>
 
-<h1>URL Shortener Microservice</h1>
-<p>Project Init</p>
+<h2 class="hero-message">Short Links. Big Impact.</h2>
+
+<style>
+  .hero-message {
+    font-family: 'Inter';
+    font-weight: 500;
+    letter-spacing: -0.045em;
+    width: 149px;
+    line-height: 32px;
+    color: #000000;
+  }
+</style>
