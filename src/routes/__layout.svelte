5ef4f09d1be7bc34a2f1a2772ccc19f8bcba44fa
Arslan <arslanastral@outlook.com>
2022-03-21 15:49:13 +0800

    added logo

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,26 @@
+<header>
+  <div class="logo-container">
+    <div class="logo-circle" />
+    <div class="logo-text">ink</div>
+  </div>
+  <nav />
+</header>
+<main />
+
+<style>
+  .logo-circle {
+    width: 70px;
+    height: 70px;
+    background: #dfed3e;
+    border-radius: 50%;
+    position: relative;
+  }
+
+  .logo-text {
+    font-size: 45px;
+    font-family: 'Merriweather';
+    position: absolute;
+    left: 16px;
+    top: 0px;
+  }
+</style>
