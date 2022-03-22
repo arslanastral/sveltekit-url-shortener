f609735bf40183321a5c3099bce8773f5334e330
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-22 17:08:10 +0800

    added POST testing function

--- src/routes/api/index.js
+++ src/routes/api/index.js
@@ -3,3 +3,13 @@ export async function get() {
     body: { status: 'working' }
   };
 }
+
+export async function post({ request }) {
+  const body = await request.formData();
+  const long_url = body.get('url');
+
+  // let long_url = request.body.get('url');
+  return {
+    body: { long_url }
+  };
+}
