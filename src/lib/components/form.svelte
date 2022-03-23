5b91c7c6b0910f577fdba5b618494845eca937b7
Arslan <arslanastral@outlook.com>
2022-03-24 00:38:24 +0800

    added URL shortening UI interaction

--- src/lib/components/form.svelte
+++ src/lib/components/form.svelte
@@ -2,6 +2,7 @@
   import Button from '$lib/components/button.svelte';
   import WebIcon from '$lib/assets/WebIcon.svelte';
   import LockIcon from '$lib/assets/LockIcon.svelte';
+  import Checkmark from '$lib/assets/Checkmark.svelte';
   let URL = '';
   let shortenedURL = '';
 
@@ -15,32 +16,64 @@
     let json = await response.json();
     shortenedURL = `${window.location.origin}/api/${json.short_url}`;
   }
+
+  const back = () => {
+    shortenedURL = '';
+    URL = '';
+  };
 </script>
 
-<div class="form-container">
-  <form class="flex form" method="post" on:submit|preventDefault={handleURLSubmit}>
+<div class="flex justify-sb form-container">
+  {#if shortenedURL}
     <div class="input-container flex">
       <div class="flex url-input">
-        <WebIcon />
-        <input bind:value={URL} placeholder="Paste Long URL Here" type="url" name="url" required />
-      </div>
-
-      <div class="flex password-input">
-        <LockIcon />
-        <input placeholder="Password" type="password" name="password" />
+        <Checkmark />
+        <div class="url-text">
+          <a href={shortenedURL} target="_blank" rel="noopener noreferrer">{shortenedURL}</a>
+        </div>
       </div>
     </div>
-
     <Button
-      title="Shortern"
-      type="submit"
+      title="Back"
+      onClickFunc={back}
+      type="button"
       --font-size="20px"
-      --padding="8px 18px"
+      --padding="8px 28px"
       --color="white"
-      --bg-color="#3E5DFF"
+      --bg-color="black"
       --border-radius="29px"
     />
-  </form>
+  {:else}
+    <form class="flex form" method="post" on:submit|preventDefault={handleURLSubmit}>
+      <div class="input-container flex">
+        <div class="flex url-input">
+          <WebIcon />
+          <input
+            bind:value={URL}
+            placeholder="Paste Long URL Here"
+            type="url"
+            name="url"
+            required
+          />
+        </div>
+
+        <div class="flex password-input">
+          <LockIcon />
+          <input placeholder="Password" type="password" name="password" />
+        </div>
+      </div>
+
+      <Button
+        title="Shortern"
+        type="submit"
+        --font-size="20px"
+        --padding="8px 18px"
+        --color="white"
+        --bg-color="#3E5DFF"
+        --border-radius="29px"
+      />
+    </form>
+  {/if}
 </div>
 
 <style>
@@ -55,6 +88,10 @@
     background: #ffffff;
     border: 1px solid rgba(0, 0, 0, 0.5);
     border-radius: 42px;
+  }
+
+  .justify-sb {
+    justify-content: space-between;
   }
 
   .form {
@@ -72,6 +109,12 @@
   input:focus {
     outline: none;
     border: 0;
+  }
+
+  .url-text {
+    font-size: 20px;
+    margin-left: 8px;
+    width: 100%;
   }
 
   .input-container {
