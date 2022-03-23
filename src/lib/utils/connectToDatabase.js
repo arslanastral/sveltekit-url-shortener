b35b7661c3d9eb2fea58e4a18bbd3c07cbb7cf0f
Arslan <arslanastral@outlook.com>
2022-03-23 20:35:20 +0800

    added mongodb connection code

---
+++ src/lib/utils/connectToDatabase.js
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb';
+
+let dbExists = null;
+
+let mongoURI = 'mongodb://localhost:27017';
+
+export const connectToDatabase = async () => {
+  if (dbExists) {
+    console.log('New Mongo Request: Using existing connection');
+    return Promise.resolve(dbExists);
+  } else {
+    return MongoClient.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+      .then((client) => {
+        let db = client.db('ky');
+        console.log('New Database Connection');
+        dbExists = db;
+        return dbExists;
+      })
+      .catch((error) => {
+        console.log('MongoDB Connection Error');
+        console.log(error);
+      });
+  }
+};
