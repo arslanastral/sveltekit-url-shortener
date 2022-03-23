93c11da737539810d65fc4f70b63e57519b2887e
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-24 00:38:24 +0800

    added URL shortening UI interaction

---
+++ src/lib/assets/Checkmark.svelte
@@ -0,0 +1,13 @@
+<span style="margin-left: 15px">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    width="31"
+    height="31"
+    viewBox="0 0 1024 1024"
+    ><path
+      fill="#8ecc5d"
+      d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896zm-56 536-99-99a38 38 0 1 0-55 54l127 127a38 38 0 0 0 54 0l263-263a38 38 0 1 0-54-54L456 600z"
+    /></svg
+  >
+</span>
