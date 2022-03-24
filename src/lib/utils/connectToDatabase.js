5b55702a73c49d5b1b1373b0916b65a216e40eae
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-24 14:24:10 +0800

    refactor database connection

--- src/lib/utils/connectToDatabase.js
+++ src/lib/utils/connectToDatabase.js
@@ -7,18 +7,18 @@ let mongoURI = 'mongodb://localhost:27017';
 export const connectToDatabase = async () => {
   if (dbExists) {
     console.log('New Mongo Request: Using existing connection');
-    return Promise.resolve(dbExists);
+    return dbExists;
   } else {
-    return MongoClient.connect(mongoURI)
-      .then((client) => {
-        let db = client.db('ky');
-        console.log('New Database Connection');
-        dbExists = db;
-        return dbExists;
-      })
-      .catch((error) => {
-        console.log('MongoDB Connection Error');
-        console.log(error);
-      });
+    try {
+      const client = new MongoClient(mongoURI);
+      await client.connect();
+      let db = client.db('ky');
+      console.log('New Database Connection');
+      dbExists = db;
+      return dbExists;
+    } catch (error) {
+      console.log('MongoDB Connection Error');
+      console.log(error);
+    }
   }
 };
