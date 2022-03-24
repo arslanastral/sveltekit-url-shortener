3e830120ad7fafcf694add6b89e00eae7de52e18
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-24 20:32:58 +0800

    improved endpoint naming

--- src/routes/api/[getshorturl].js
+++ src/routes/api/[getshorturl].js
@@ -1,12 +1,12 @@
 import { connectToDatabase } from '$lib/utils/connectToDatabase.js';
 
 export async function get({ params }) {
-  const shorturlID = params.shorturl;
+  const id = params.getshorturl;
 
   try {
     const db = await connectToDatabase();
     const collection = await db.collection('urls');
-    const link = await collection.findOne({ short_url: shorturlID });
+    const link = await collection.findOne({ short_url: id });
 
     return {
       headers: { Location: link.long_url },
