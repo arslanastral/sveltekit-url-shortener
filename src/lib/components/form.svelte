163013f7775ffd6d9e5ff0bc3041e30268a79596
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-24 20:50:52 +0800

    removed unused code

--- src/lib/components/form.svelte
+++ src/lib/components/form.svelte
@@ -22,37 +22,6 @@
     shortenedURL = '';
     URL = '';
   };
-
-  export function clickToCopy(node, target) {
-    async function copyText() {
-      let text = target ? document.querySelector(target).innerText : node.innerText;
-
-      try {
-        await navigator.clipboard.writeText(text);
-
-        node.dispatchEvent(
-          new CustomEvent('copysuccess', {
-            bubbles: true
-          })
-        );
-      } catch (error) {
-        node.dispatchEvent(
-          new CustomEvent('copyerror', {
-            bubbles: true,
-            detail: error
-          })
-        );
-      }
-    }
-
-    node.addEventListener('click', copyText);
-
-    return {
-      destroy() {
-        node.removeEventListener('click', copyText);
-      }
-    };
-  }
 </script>
 
 <div class="flex justify-sb form-container">
