38785b913297823f9a4c2d8b2c88f38565ce4d4c
Arslan <arslanastral@outlook.com>
2022-03-27 12:35:06 +0800

    moved error handling to component

---
+++ src/lib/components/Error.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Button from './Button.svelte';
+  export let error = '';
+</script>
+
+<div class="flex grow">
+  <span class="error grow">{error}</span><Button
+    title="Retry"
+    type="submit"
+    --font-size="20px"
+    --padding="8px 18px"
+    --color="white"
+    --bg-color="black"
+    --border-radius="29px"
+  />
+</div>
+
+<style>
+  .error {
+    font-size: 20px;
+    color: red;
+    margin-left: 15px;
+  }
+</style>
