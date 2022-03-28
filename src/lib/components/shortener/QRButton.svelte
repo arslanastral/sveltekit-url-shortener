7de4cadc48d8739f3cfab45847179252dbe42368
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-28 14:34:35 +0800

    added QR button component

---
+++ src/lib/components/shortener/QRButton.svelte
@@ -0,0 +1,26 @@
+<script>
+</script>
+
+<button type="button" class="flex"><span>{'QR'}</span></button>
+
+<style>
+  button {
+    font-size: 20px;
+    padding: 8px 18px;
+    color: black;
+    background-color: white;
+    border-radius: 29px;
+    border: 1px solid #808080;
+    margin: 6px;
+    cursor: pointer;
+    transition: transform ease-in 0.1s;
+  }
+
+  button:active {
+    transform: scale(0.9);
+  }
+
+  span {
+    margin-left: 3px;
+  }
+</style>
