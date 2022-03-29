2667f3a9b9d13679588daa09a1a274a346adae3e
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-29 20:13:52 +0800

    added stats box styling

--- src/lib/components/stats/StatsBox.svelte
+++ src/lib/components/stats/StatsBox.svelte
@@ -1,4 +1,16 @@
-<div class="stats-box" />
+<script>
+  export let count = Math.floor(Math.random() * 200);
+  export let countType;
+</script>
+
+<div class="flex stats-box">
+  <div class="flex info grow">
+    <div class="flex info-wrapper">
+      <span class="count">{count}</span><span class="count-type">{countType}</span>
+    </div>
+  </div>
+  <div class="grow"><slot name="icon" /></div>
+</div>
 
 <style>
   .stats-box {
@@ -7,5 +19,24 @@
     background: #ffffff;
     border: 1px solid #dfe1e5;
     border-radius: 19px;
+  }
+
+  .info {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .count {
+    font-weight: 500;
+    font-size: 59px;
+    line-height: 55px;
+    letter-spacing: -0.045em;
+
+    color: var(--count-color);
+  }
+
+  .info-wrapper {
+    flex-direction: column;
+    align-items: flex-start;
   }
 </style>
