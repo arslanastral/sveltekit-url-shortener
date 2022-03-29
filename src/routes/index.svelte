f87bc7ae549fa171c6417048cc7285d222ca65a4
Arslan <arslanastral@outlook.com>
2022-03-29 18:18:20 +0800

    added stats container

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,13 @@
 <script>
   import Hero from '$lib/components/Hero.svelte';
   import Shortener from '$lib/components/shortener/Shortener.svelte';
+  import Stats from '$lib/components/stats/Stats.svelte';
 </script>
 
 <div class="content">
   <Hero />
   <Shortener />
+  <Stats />
 </div>
 
 <style>
