3405ebe169e5b03cfd78411efcb5018c7fc6f954
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-03-29 18:18:20 +0800

    added stats container

---
+++ src/lib/components/stats/Stats.svelte
@@ -0,0 +1,12 @@
+<div class="stats-container" />
+
+<style>
+  .stats-container {
+    width: clamp(320px, 90vw, 800px);
+    height: 200px;
+    background: #feffb1;
+    /* border: 1px solid #dfe1e5; */
+    border-radius: 12px;
+    margin-top: 50px;
+  }
+</style>
