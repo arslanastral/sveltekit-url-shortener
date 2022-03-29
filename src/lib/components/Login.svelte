4e91ce6fb39f945a42cb8f6c38f34470bb1556c7
Arslan <arslanastral@outlook.com>
2022-03-29 12:54:20 +0800

    styling tweaks

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="login-container">
-  <Button title="Log In" --font-size="18px" --padding="8px 14px" --border="1px solid #000000" />
+  <Button title="Log In" --font-size="18px" --padding="8px 14px" --border="1px solid #dfe1e5" />
 
   <Button
     title="Sign Up"
