39b909aa5c8f57ea7dba8934639d301bd526abbc
Arslan <arslanastral@outlook.com>
2022-03-30 19:00:39 +0800

    implemented stats endpoint

--- src/routes/api/stats/index.js
+++ src/routes/api/stats/index.js
@@ -1,6 +1,43 @@
+import { connectToDatabase } from '$lib/utils/connectToDatabase.js';
+
 export async function get() {
-  return {
-    body: 'Stats Working',
-    status: 200
-  };
+  try {
+    const db = await connectToDatabase();
+    const collection = await db.collection('urls');
+
+    let shortened = await collection.countDocuments({ short_url: { $exists: true } });
+
+    const clicksPipeline = [
+      {
+        $group: {
+          _id: '',
+          clicks: {
+            $sum: '$clicks'
+          }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          clicks: '$clicks'
+        }
+      }
+    ];
+
+    const aggregateClicks = await collection.aggregate(clicksPipeline).toArray();
+
+    let clicks = aggregateClicks[0].clicks;
+
+    const secured = await collection.countDocuments({ secured: { $eq: true } });
+
+    return {
+      body: { shortened: shortened, clicks: clicks, secured: secured },
+      status: 200
+    };
+  } catch (error) {
+    return {
+      body: 'Error Getting Stats',
+      status: 500
+    };
+  }
 }
