1a365d31261b97f1b35ee4161ce6eae09755558e
Arslan <arslanastral@outlook.com>
2022-03-31 19:56:17 +0800

    added unlock component

---
+++ src/lib/components/unlock/Unlock.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let id;
+</script>
+
+<div class="unlock-container">
+  <span>Your ID: {id} ✨</span>
+</div>
+
+<style>
+  .unlock-container {
+    background-color: white;
+    width: 386px;
+    height: 494px;
+    box-shadow: 0px 0px 15px -2px rgba(0, 0, 0, 0.25);
+    border-radius: 18px;
+  }
+</style>
