5a9440609193a763cce183552d08066143b0d499
Arslan <arslanastral@outlook.com>
2022-03-31 14:51:18 +0800

    handled password protected links

--- src/routes/api/shorturl/index.js
+++ src/routes/api/shorturl/index.js
@@ -1,25 +1,54 @@
 import { connectToDatabase } from '$lib/utils/connectToDatabase.js';
 import { isValidHttpUrl } from '$lib/utils/isValidHttpUrl.js';
+import argon2 from 'argon2';
 import { nanoid } from 'nanoid';
 
 export async function post({ request }) {
   const body = await request.formData();
   const submittedURL = body.get('url');
+  const submittedPassword = body.get('password');
   const host = request.headers.get('host');
+  console.log(submittedURL);
+  console.log(submittedPassword);
 
   if (isValidHttpUrl(submittedURL, host)) {
     try {
       const db = await connectToDatabase();
       const collection = await db.collection('urls');
+      let shortened;
+      if (submittedPassword) {
+        try {
+          const hash = await argon2.hash(submittedPassword);
 
-      const shortened = await collection.insertOne({
-        long_url: submittedURL,
-        short_url: nanoid(4),
-        clicks: 0
-      });
+          shortened = await collection.insertOne({
+            long_url: submittedURL,
+            short_url: nanoid(4) + 'L',
+            clicks: 0,
+            secured: true,
+            pass: hash
+          });
+        } catch (err) {
+          return {
+            status: 500,
+            body: { error: 'Error Creating Secure Link' }
+          };
+        }
+      } else {
+        shortened = await collection.insertOne({
+          long_url: submittedURL,
+          short_url: nanoid(4),
+          clicks: 0
+        });
+      }
 
       const shortenedResult = await collection.findOne({ _id: shortened.insertedId });
 
+      if (submittedPassword) {
+        return {
+          status: 200,
+          body: { short_url: shortenedResult.short_url }
+        };
+      }
       return {
         status: 200,
         body: { short_url: shortenedResult.short_url, long_url: shortenedResult.long_url }
