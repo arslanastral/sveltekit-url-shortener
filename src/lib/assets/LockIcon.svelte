5fb16e3dae93de24a1eab125e7b2ff4ed6fc67d5
Arslan <arslanastral@outlook.com>
2022-04-01 11:48:24 +0800

    exported Icon props

--- src/lib/assets/LockIcon.svelte
+++ src/lib/assets/LockIcon.svelte
@@ -1,11 +1,12 @@
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  aria-hidden="true"
-  width="25"
-  height="25"
-  viewBox="0 0 1024 1024"
+<script>
+  export let width = '25';
+  export let height = '25';
+  export let fill = '#ccc';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {width} {height} viewBox="0 0 1024 1024"
   ><path
-    fill="#ccc"
+    {fill}
     d="M832 464h-68V240c0-71-57-128-128-128H388c-71 0-128 57-128 128v224h-68c-18 0-32 14-32 32v384c0 18 14 32 32 32h640c18 0 32-14 32-32V496c0-18-14-32-32-32zM540 701v53c0 4-4 8-8 8h-40c-4 0-8-4-8-8v-53a48 48 0 1 1 56 0zm152-237H332V240c0-31 25-56 56-56h248c31 0 56 25 56 56v224z"
   /></svg
 >
