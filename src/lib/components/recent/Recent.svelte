58aaa4d522ad202f6cfee7bdd0160784e30adc4e
Arslan <arslanastral@outlook.com>
2022-04-01 17:06:24 +0800

    added links container

--- src/lib/components/recent/Recent.svelte
+++ src/lib/components/recent/Recent.svelte
@@ -1,16 +1,23 @@
 <div class="recent-container">
-  <div>Hello</div>
+  <div class="flex"><span class="stats-title">Recently Shortened</span></div>
+  <div class="recent-links-container" />
 </div>
 
 <style>
   .recent-container {
     width: clamp(320px, 90vw, 800px);
     height: 250px;
-    background: #3e5dff;
-    border-radius: 12px;
+
     margin-top: 50px;
     flex-direction: column;
     align-items: flex-start;
     justify-content: space-evenly;
+  }
+
+  .recent-links-container {
+    background: #3e5dff;
+    border-radius: 12px;
+    width: 100%;
+    height: 90%;
   }
 </style>
