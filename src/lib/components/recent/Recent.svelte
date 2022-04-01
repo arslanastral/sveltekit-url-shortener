c3ae775e76e91e4a9b84a2d2b3fb2d25a258d778
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-01 17:00:56 +0800

    added recents component

---
+++ src/lib/components/recent/Recent.svelte
@@ -0,0 +1,16 @@
+<div class="recent-container">
+  <div>Hello</div>
+</div>
+
+<style>
+  .recent-container {
+    width: clamp(320px, 90vw, 800px);
+    height: 250px;
+    background: #3e5dff;
+    border-radius: 12px;
+    margin-top: 50px;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-evenly;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,12 +2,14 @@
   import Hero from '$lib/components/Hero.svelte';
   import Shortener from '$lib/components/shortener/Shortener.svelte';
   import Stats from '$lib/components/stats/Stats.svelte';
+  import Recent from '$lib/components/recent/Recent.svelte';
 </script>
 
 <div class="content">
   <Hero />
   <Shortener />
   <Stats />
+  <Recent />
 </div>
 
 <style>
