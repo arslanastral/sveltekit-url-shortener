f05fead3773331cfd2d872eb52032ad6e02da101
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-02 17:37:11 +0800

    created link button component

---
+++ src/lib/components/recent/LinkButton.svelte
@@ -0,0 +1,21 @@
+<div class="flex">
+  <button class="qr-button" />
+  <span class="button-seperator" />
+  <button class="copy-button" />
+</div>
+
+<style>
+  div {
+    background: #ffffff;
+    border: 1px solid #dfe1e5;
+    border-radius: 5px;
+    padding: 8px 18px;
+    justify-content: space-evenly;
+  }
+
+  .button-seperator {
+    height: 100%;
+    width: 1px;
+    background-color: #dfe1e5;
+  }
+</style>
