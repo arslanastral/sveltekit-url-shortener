2d7b2296526109f0f845af634a68ad4f9dab70c4
Arslan <arslanastral@outlook.com>
2022-04-02 17:29:12 +0800

    added recent link component

---
+++ src/lib/components/recent/RecentLink.svelte
@@ -0,0 +1,22 @@
+<div class="linkbox flex">
+  <div>2m ago</div>
+  <div class="flex link">
+    <span>https://developer.mozilla.org/en-US/</span><span>http://localhost:3000/rpPt</span>
+  </div>
+  <div />
+</div>
+
+<style>
+  .linkbox {
+    margin: 10px;
+    height: 60px;
+    background: rgb(60, 60, 255);
+    color: black;
+    justify-content: space-between;
+  }
+
+  .link {
+    flex-direction: column;
+    align-items: flex-start;
+  }
+</style>
