93214b92719701c37942da50cc01fc1baa221cb1
Arslan <arslanastral@outlook.com>
2022-04-03 17:50:35 +0800

    moved QRCode to another file

---
+++ src/lib/utils/generateQRCode.js
@@ -0,0 +1,9 @@
+export async function generateQRCode(text, size = 200) {
+  const { default: kjua } = await import('kjua');
+  return kjua({
+    text,
+    fill: 'blue',
+    rounded: 100,
+    size
+  });
+}
