3ac685ce3a2a992a1f4d3cda3747cc78d1b6b078
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-05 13:01:02 +0800

    connected login buttons with login routes

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -3,22 +3,26 @@
 </script>
 
 <div class="login-container">
-  <Button
-    title="Log In"
-    --font-size="18px"
-    --padding="8px 14px"
-    --border="1px solid #dfe1e5"
-    --hover-border="1px solid #444"
-  />
+  <a href="/signin">
+    <Button
+      title="Log In"
+      --font-size="18px"
+      --padding="8px 14px"
+      --border="1px solid #dfe1e5"
+      --hover-border="1px solid #444"
+    />
+  </a>
 
-  <Button
-    title="Sign Up"
-    --font-size="18px"
-    --bg-color="#DFED3E"
-    --padding="8px 12px"
-    --border="none"
-    --active-bg="#eeff2a"
-  />
+  <a href="/signup">
+    <Button
+      title="Sign Up"
+      --font-size="18px"
+      --bg-color="#DFED3E"
+      --padding="8px 12px"
+      --border="none"
+      --active-bg="#eeff2a"
+    />
+  </a>
 </div>
 
 <style>
