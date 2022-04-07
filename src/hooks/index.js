a6f74a798af27247bf3ca7e3a9eef8b2336c3c13
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-07 18:39:01 +0800

    implemented hooks cookie sesssion

---
+++ src/hooks/index.js
@@ -0,0 +1,45 @@
+import * as cookie from 'cookie';
+import { connectToDatabase } from '$lib/utils/connectToDatabase.js';
+
+export async function handle({ event, resolve }) {
+  const cookies = cookie.parse(event.request.headers.get('cookie') || '');
+
+  event.locals.user = cookies;
+
+  if (!cookies.sessionId) {
+    event.locals.user.authenticated = false;
+  }
+
+  const db = await connectToDatabase();
+  const collection = await db.collection('users');
+
+  const userSession = await collection.findOne({ sessionId: cookies.sessionId });
+
+  if (userSession) {
+    event.locals.user.authenticated = true;
+    event.locals.user.email = userSession.email;
+  } else {
+    event.locals.user.authenticated = false;
+  }
+
+  const response = await resolve(event);
+
+  return {
+    ...response,
+    headers: {
+      ...response.headers
+    }
+  };
+}
+
+export const getSession = async (event) => {
+  return event.locals.user
+    ? {
+        user: {
+          authenticated: true,
+          name: event.locals.user.name,
+          email: event.locals.user.email
+        }
+      }
+    : {};
+};
