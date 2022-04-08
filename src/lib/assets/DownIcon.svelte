5bcb5518200f579bc95e6aff573d6b8e9776d08a
Arslan <arslanastral@outlook.com>
2022-04-08 21:50:40 +0800

    added dropdown button

---
+++ src/lib/assets/DownIcon.svelte
@@ -0,0 +1,8 @@
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" width="24" height="24"
+  ><path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M12 1a11 11 0 1 0 0 22 11 11 0 0 0 0-22Zm-3.2 9a.8.8 0 0 0-.7.4.71.71 0 0 0 .04.77l3.2 4.5A.81.81 0 0 0 12 16a.81.81 0 0 0 .67-.33l3.2-4.5a.71.71 0 0 0 .04-.77.8.8 0 0 0-.71-.4H8.8Z"
+    clip-rule="evenodd"
+  /></svg
+>
--- src/lib/components/user/UserInfo.svelte
+++ src/lib/components/user/UserInfo.svelte
@@ -1,12 +1,28 @@
 <script>
+  import DownIcon from '$lib/assets/DownIcon.svelte';
+
   export let name;
 </script>
 
-<div class="user-name">Hey, {name}</div>
+<div class="flex">
+  <div class="user-name">Hey, {name}</div>
+  <button><DownIcon /></button>
+</div>
 
 <style>
   .user-name {
     font-size: 20px;
     letter-spacing: -0.045em;
+  }
+
+  button {
+    margin-left: 10px;
+    padding: 0;
+    border: none;
+    transition: transform ease-out 0.1s;
+  }
+
+  button:active {
+    transform: scale(0.9);
   }
 </style>
