f86c2c2adf2852cfb8ccb1f8a7b3aeaa3d90cca3
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-13 00:00:55 +0800

    refactor shortener endpoint

--- src/routes/api/shorturl/index.js
+++ src/routes/api/shorturl/index.js
@@ -1,8 +1,6 @@
-import { useCollection } from '$lib/utils/useCollection';
-import { isValidHttpUrl } from '$lib/utils/isValidHttpUrl.js';
 import rateLimiter from 'lambda-rate-limiter';
-import bcrypt from 'bcryptjs';
-import { nanoid } from 'nanoid';
+import { isValidHttpUrl } from '$lib/utils/isValidHttpUrl.js';
+import { createShortUrl } from '$lib/services/createShortUrl';
 
 const limiter = rateLimiter({
   interval: 120000
@@ -24,69 +22,12 @@ export async function post({ clientAddress, request }) {
   }
 
   if (isValidHttpUrl(submittedURL, host)) {
-    try {
-      const collection = await useCollection('urls');
-      let shortened;
-      if (submittedPassword) {
-        try {
-          const hash = await bcrypt.hash(submittedPassword, 10);
+    const result = await createShortUrl(submittedURL, submittedPassword);
 
-          shortened = await collection.insertOne({
-            long_url: submittedURL,
-            short_url: nanoid(4),
-            clicks: 0,
-            secured: true,
-            pass: hash
-          });
-        } catch (err) {
-          return {
-            status: 500,
-            body: { error: 'Error Creating Secure Link' }
-          };
-        }
-      } else {
-        shortened = await collection.insertOne({
-          long_url: submittedURL,
-          short_url: nanoid(4),
-          clicks: 0
-        });
-      }
-
-      const shortenedResult = await collection.findOne({ _id: shortened.insertedId });
-
-      const created_at = shortenedResult._id.getTimestamp().toString();
-
-      if (submittedPassword) {
-        return {
-          status: 200,
-          body: {
-            short_url: shortenedResult.short_url,
-            secured: shortenedResult.secured,
-            created_at
-          }
-        };
-      }
-      return {
-        status: 200,
-        body: {
-          short_url: shortenedResult.short_url,
-          long_url: shortenedResult.long_url,
-          created_at
-        }
-      };
-    } catch (error) {
-      if (error.code === 11000) {
-        return {
-          body: { error: 'Short Link Already Exists' },
-          status: 400
-        };
-      }
-
-      return {
-        body: { error: 'Something Went Wrong' },
-        status: 500
-      };
-    }
+    return {
+      status: result.status,
+      body: result.body
+    };
   }
 
   return {
