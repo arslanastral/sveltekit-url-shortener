46cf9b362ac0bf40519d6618f66f8e09f0f7552e
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-13 16:59:06 +0800

    created dashboard component

---
+++ src/lib/components/dashboard/Dashboard.svelte
@@ -0,0 +1,22 @@
+<div class="flex title-container">
+  <div class="title">Dashboard</div>
+</div>
+<div class="grow links-container">Links</div>
+
+<style>
+  .title-container {
+    width: 80%;
+  }
+  .title {
+    font-size: 35px;
+    font-weight: bold;
+    margin: 10px 0;
+  }
+  .links-container {
+    width: 80%;
+    margin-bottom: 20px;
+    background: #ffffff;
+    box-shadow: 0px 0px 4px -1px rgba(0, 0, 0, 0.25);
+    border-radius: 24px;
+  }
+</style>
