0ad32fabccccb7bb4cadb51c535b0bc9a2f0fea0
Arslan <arslanastral@outlook.com>
2022-04-13 16:59:06 +0800

    created dashboard component

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,1 +1,20 @@
-<div>Dashboard</div>
+<script>
+  import Dashboard from '$lib/components/dashboard/Dashboard.svelte';
+</script>
+
+<div class="wrapper">
+  <Dashboard />
+</div>
+
+<style>
+  .wrapper {
+    background-repeat: repeat;
+    background-color: #fafafa;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    min-height: 90vh;
+    min-width: 100vw;
+  }
+</style>
