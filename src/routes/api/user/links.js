00f03a1c97f79ca07f0ce5ad2cd54a3d91581644
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-15 17:14:55 +0800

    implemented user links route

---
+++ src/routes/api/user/links.js
@@ -0,0 +1,32 @@
+import { useCollection } from '$lib/utils/useCollection';
+
+export async function get({ locals }) {
+  const currentUser = locals.user;
+
+  if (!currentUser.authenticated) {
+    return {
+      status: 401,
+      body: { error: 'Unauthorized' }
+    };
+  }
+
+  try {
+    const collection = await useCollection('urls');
+    const links = await collection
+      .find({ created_by: currentUser.email })
+      .project({ _id: 0, long_url: 1, short_url: 1, secured: 1, clicks: 1 })
+      .toArray();
+
+    return {
+      status: 200,
+      body: {
+        links: links
+      }
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      body: { error: 'Internal Server Error' }
+    };
+  }
+}
