92a9888fde922f8703e7986bfc15ae435e5e2852
Arslan <arslanastral@outlook.com>
2022-04-15 12:39:01 +0800

    added dashboard link styling

--- src/lib/components/dashboard/Dashboard.svelte
+++ src/lib/components/dashboard/Dashboard.svelte
@@ -1,7 +1,16 @@
 <div class="flex title-container">
   <div class="title">Dashboard</div>
 </div>
-<div class="grow links-container">Links</div>
+<div class="grow links-wrapper">
+  <div class="flex link-container">
+    <span class="index">1.</span>
+    <div class="link">
+      <div class="long-link">https://www.passportjs.org/packages...</div>
+      <div class="short-link">ky.vercel.app/b4w7</div>
+    </div>
+    <div class="date">22 Nov, 2022</div>
+  </div>
+</div>
 
 <style>
   .title-container {
@@ -12,11 +21,38 @@
     font-weight: bold;
     margin: 10px 0;
   }
-  .links-container {
+  .links-wrapper {
     width: 80%;
     margin-bottom: 20px;
     background: #ffffff;
     box-shadow: 0px 0px 4px -1px rgba(0, 0, 0, 0.25);
     border-radius: 24px;
+  }
+
+  .link-container {
+    width: 100%;
+  }
+
+  .index {
+    margin: 20px;
+    font-weight: 500;
+    font-size: 19px;
+  }
+
+  .link {
+    margin: 20px;
+  }
+
+  .short-link {
+    font-weight: 500;
+    font-size: 19px;
+  }
+
+  .date {
+    margin: 20px;
+
+    font-size: 17px;
+
+    color: #555555;
   }
 </style>
