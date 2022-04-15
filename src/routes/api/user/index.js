fd4d4547d6f44f9c9b2f8d37e719753715fab54f
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-15 16:34:54 +0800

    added user api route

---
+++ src/routes/api/user/index.js
@@ -0,0 +1,20 @@
+export async function get({ locals }) {
+  const currentUser = locals.user;
+
+  if (!currentUser.authenticated) {
+    return {
+      status: 401,
+      body: { error: 'Unauthorized' }
+    };
+  }
+
+  return {
+    status: 200,
+    body: {
+      authenticated: currentUser.authenticated,
+      name: currentUser.name,
+      email: currentUser.email,
+      created_at: currentUser.createdAt
+    }
+  };
+}
