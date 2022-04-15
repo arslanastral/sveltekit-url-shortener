f9c39f5b1f541a2a9c5c931f3ba3c4ceb33b6a63
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-15 23:40:10 +0800

    stats styling

--- src/lib/components/stats/Stats.svelte
+++ src/lib/components/stats/Stats.svelte
@@ -30,11 +30,7 @@
 <style>
   .stats-container {
     width: clamp(320px, 90vw, 800px);
-    /* height: 200px; */
-    /* background: #feffb1; */
-    /* border: 1px solid #dfe1e5; */
     border-radius: 12px;
-    margin-top: 50px;
     flex-direction: column;
     align-items: flex-start;
     justify-content: space-evenly;
