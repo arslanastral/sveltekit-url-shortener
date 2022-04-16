ec83dd868632012e3123d7ca0971d41e6b1c6dae
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-16 22:11:25 +0800

    added edit icon

---
+++ src/lib/assets/EditIcon.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let width = '20';
+  export let height = '20';
+  export let fill = '#000';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {width} {height} viewBox="0 0 24 24"
+  ><path
+    {fill}
+    d="M4.42 20.58a1 1 0 0 1-.74-.33.99.99 0 0 1-.26-.76l.24-2.7L14.98 5.5l3.54 3.53L7.21 20.33l-2.7.24a.95.95 0 0 1-.09 0ZM19.22 8.3 15.7 4.77l2.12-2.12a1 1 0 0 1 1.42 0l2.12 2.12a1 1 0 0 1 0 1.42L19.23 8.3Z"
+  /></svg
+>
