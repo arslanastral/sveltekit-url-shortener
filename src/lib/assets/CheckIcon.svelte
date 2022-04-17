fcdfa5ab853817b69ada0a52e1c8209c275e7e12
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-17 20:23:51 +0800

    checkmark icon size

--- src/lib/assets/CheckIcon.svelte
+++ src/lib/assets/CheckIcon.svelte
@@ -6,3 +6,9 @@
   viewBox="0 0 24 24"
   ><path fill="currentColor" d="m10 15.59-3.3-3.3-1.4 1.42 4.7 4.7 9.7-9.7-1.4-1.42z" /></svg
 >
+
+<style>
+  svg {
+    transform: scale(1.5);
+  }
+</style>
