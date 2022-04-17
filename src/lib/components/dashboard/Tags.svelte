625aa1e460b7d63e32ac59509ec88691c25b31d8
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-17 14:00:28 +0800

    added tags component

---
+++ src/lib/components/dashboard/Tags.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let tag;
+</script>
+
+<span class="tag">{tag}</span>
+
+<style>
+  .tag {
+    margin: 5px 5px 5px 0;
+    border-radius: 20px;
+    background-color: var(--bg-color);
+    color: var(--color);
+    padding: 2px 15px;
+  }
+</style>
