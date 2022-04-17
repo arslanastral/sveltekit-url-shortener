178baa6d5e6a502d5898f00605fc6c5043acaf84
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-17 19:25:42 +0800

    added tag icon

---
+++ src/lib/assets/TagIcon.svelte
@@ -0,0 +1,13 @@
+<span style="margin-left: 10px;">
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    aria-hidden="true"
+    width="30"
+    height="30"
+    viewBox="0 0 24 24"
+    ><path
+      fill="#ccc"
+      d="M12.6 2.6a2 2 0 0 0-1.4-.6H4a2 2 0 0 0-2 2v7.2a2 2 0 0 0 .6 1.4l8 8a2 2 0 0 0 2.8 0l7.2-7.2a2 2 0 0 0 0-2.8l-8-8zM7 9a2 2 0 1 1 0-4 2 2 0 0 1 0 4z"
+    /></svg
+  >
+</span>
