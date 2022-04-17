9fba6d1dec64a70434b209348f5b45c2248e7cf4
Arslan <arslanastral@outlook.com>
2022-04-17 20:08:17 +0800

    moved tag code to own component

---
+++ src/lib/assets/CheckIcon.svelte
@@ -0,0 +1,8 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  aria-hidden="true"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  ><path fill="currentColor" d="m10 15.59-3.3-3.3-1.4 1.42 4.7 4.7 9.7-9.7-1.4-1.42z" /></svg
+>
