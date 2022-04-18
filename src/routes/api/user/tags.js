08127a8aa4f7843c73c0d33e5e0cbd7aa5b42387
Arslan <arslanastral@outlook.com>
2022-04-18 16:42:20 +0800

    implemented tags endpoint

---
+++ src/routes/api/user/tags.js
@@ -0,0 +1,46 @@
+import { useCollection } from '$lib/utils/useCollection';
+
+export async function patch({ locals, request }) {
+  const currentUser = locals.user;
+
+  if (!currentUser.authenticated) {
+    return {
+      status: 401,
+      body: { error: 'Unauthorized' }
+    };
+  }
+
+  const body = await request.json();
+  const { short_url, tags } = await body;
+
+  try {
+    const collection = await useCollection('urls');
+
+    const link = await collection.find({ short_url: short_url });
+
+    if (link) {
+      let updatedLink = await collection.updateOne(
+        { short_url: short_url },
+        { $set: { tags: tags } }
+      );
+
+      return {
+        status: 200,
+        body: {
+          short_url: updatedLink.short_url,
+          long_url: updatedLink.long_url,
+          tags: updatedLink.tags
+        }
+      };
+    }
+    return {
+      status: 400,
+      body: { error: 'No such link' }
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      body: { error: 'Internal Server Error' }
+    };
+  }
+}
