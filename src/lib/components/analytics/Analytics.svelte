8eca7d9415581a7cb54353da9438502cffe96ef9
Arslan <arslanastral@outlook.com>
2022-04-21 19:01:01 +0800

    added analytics component

---
+++ src/lib/components/analytics/Analytics.svelte
@@ -0,0 +1,18 @@
+<div class="flex container">
+  <div class="main-title">Analytics</div>
+</div>
+
+<style>
+  .container {
+    width: 80%;
+    margin-top: 20px;
+    flex-direction: column;
+  }
+
+  .main-title {
+    width: 100%;
+    font-size: 35px;
+    font-weight: bold;
+    margin: 10px 0;
+  }
+</style>
--- src/routes/analytics.svelte
+++ src/routes/analytics.svelte
@@ -1,5 +1,6 @@
 <script context="module">
-  export async function load({ session }) {
+  import { getStats } from '$lib/utils/getStats';
+  export async function load({ session, fetch }) {
     if (!session.user) {
       return {
         status: 302,
@@ -10,4 +11,21 @@
   }
 </script>
 
-<div>Not Implemented Yet</div>
+<script>
+  import Analytics from '$lib/components/analytics/Analytics.svelte';
+</script>
+
+<div class="fadeIn wrapper">
+  <Analytics />
+</div>
+
+<style>
+  .wrapper {
+    background-repeat: repeat;
+    background-color: #fafafa;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    min-height: 90vh;
+  }
+</style>
