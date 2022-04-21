94e6c306832a49e403ec9225ce62825786af7f44
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-21 18:26:27 +0800

    created setting component

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -1,5 +1,6 @@
 <script context="module">
-  export async function load({ session }) {
+  import { getStats } from '$lib/utils/getStats';
+  export async function load({ session, fetch }) {
     if (!session.user) {
       return {
         status: 302,
@@ -10,4 +11,22 @@
   }
 </script>
 
-<div>Not Implemented Yet</div>
+<script>
+  import Settings from '$lib/components/settings/Settings.svelte';
+</script>
+
+<div class="fadeIn wrapper">
+  <Settings />
+</div>
+
+<style>
+  .wrapper {
+    background-repeat: repeat;
+    background-color: #fafafa;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    min-height: 90vh;
+  }
+</style>
