9a1adafc04a45a8e32b7b91d01e9eb10da9c3735
Arslan <arslanastral@outlook.com>
2022-04-22 23:50:31 +0800

    added settings container

--- src/lib/components/settings/Settings.svelte
+++ src/lib/components/settings/Settings.svelte
@@ -1,5 +1,6 @@
 <div class="flex container">
   <div class="main-title">Settings</div>
+  <div class="settings-container flex"><span>In Progress</span></div>
 </div>
 
 <style>
@@ -14,5 +15,16 @@
     font-size: 35px;
     font-weight: bold;
     margin: 10px 0;
+  }
+
+  .settings-container {
+    width: 100%;
+    height: 700px;
+    margin-top: 30px;
+    background-color: white;
+    box-shadow: 0px 0px 4px -1px rgba(0, 0, 0, 0.25);
+    border-radius: 24px;
+    overflow: hidden;
+    justify-content: center;
   }
 </style>
