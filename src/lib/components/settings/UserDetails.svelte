410b7f343116cb33e4097b717726d94281872c41
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-23 16:33:44 +0800

    added user info markup

---
+++ src/lib/components/settings/UserDetails.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { session } from '$app/stores';
+</script>
+
+<div class="user-info flex">
+  <div>Welcome, {session.user.name}</div>
+  <div class="user-info__name flex">
+    <span>Name</span>
+    <h1>{$session.user.name}</h1>
+  </div>
+
+  <div class="user-info__email flex">
+    <span>Name</span>
+    <h2>{$session.user.email}</h2>
+  </div>
+</div>
+
+<style>
+  .user-info {
+    flex-direction: column;
+  }
+</style>
