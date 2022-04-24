d42eb57a9d50ae7e38ef1bbd2c80b8caf0f6628b
Arslan <arslanastral@outlook.com>
2022-04-24 15:53:10 +0800

    added password update endpoint

---
+++ src/routes/api/user/account/password.js
@@ -0,0 +1,59 @@
+import { useCollection } from '$lib/utils/useCollection';
+import bcryptjs from 'bcryptjs';
+
+export async function put({ locals, request }) {
+  const body = await request.json();
+  const { oldPass, newPass } = await body;
+  const currentUser = locals.user.email || null;
+
+  if (!currentUser) {
+    return {
+      status: 400,
+      body: {
+        error: 'Not logged in'
+      }
+    };
+  }
+
+  try {
+    const collection = await useCollection('users');
+
+    const user = await collection.findOne({ email: { $eq: currentUser } });
+
+    if (user) {
+      if (await bcryptjs.compare(oldPass, user.password)) {
+        const hash = await bcryptjs.hash(newPass, 10);
+
+        await collection.updateOne(
+          { email: currentUser },
+          {
+            $set: {
+              password: hash
+            }
+          }
+        );
+
+        return {
+          status: 200,
+          body: {
+            status: 'Password Changed Successfully'
+          }
+        };
+      }
+
+      return {
+        status: 200,
+        body: {
+          status: 'Password Not Matched'
+        }
+      };
+    }
+  } catch (error) {
+    return {
+      status: 500,
+      body: {
+        error: 'Internal server error'
+      }
+    };
+  }
+}
