4a0f56edeeea46edb6b27b0c80fd890410eb053f
Arslan <arslanastral@outlook.com>
2022-04-25 16:02:10 +0800

    added clicks icon

---
+++ src/lib/assets/ClicksIcon.svelte
@@ -0,0 +1,12 @@
+<script>
+  export let width = '35';
+  export let height = '35';
+  export let fill = 'currentColor';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {width} {height} viewBox="0 0 24 24"
+  ><path
+    {fill}
+    d="M12 4a1 1 0 0 1 1 1v14a1 1 0 1 1-2 0V5a1 1 0 0 1 1-1zm5 4a1 1 0 0 1 1 1v10a1 1 0 1 1-2 0V9a1 1 0 0 1 1-1zM7 12a1 1 0 0 1 1 1v6a1 1 0 1 1-2 0v-6a1 1 0 0 1 1-1z"
+  /></svg
+>
