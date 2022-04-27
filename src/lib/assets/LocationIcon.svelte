fe900a37fa81fe61aa0a1d3ef27ad8a765d4388d
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-27 23:13:01 +0800

    added location icon

---
+++ src/lib/assets/LocationIcon.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let width = '40';
+  export let height = '40';
+  export let fill = 'currentColor';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {width} {height} viewBox="0 0 48 48"
+  ><path
+    fill="none"
+    stroke={fill}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M24 4.5A14.82 14.82 0 0 0 9.18 19.32h0v1.08c.6 8.12 7.34 14.65 14.82 23.1 7.81-8.82 14.82-15.5 14.82-24.18h0A14.82 14.82 0 0 0 24 4.5Zm0 7.7a7.13 7.13 0 1 1-7.13 7.12A7.13 7.13 0 0 1 24 12.2Z"
+  /></svg
+>
