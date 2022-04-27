397af4158432518a15cffd42fc9690ae906014d0
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-27 23:21:52 +0800

    added icons to data points

--- src/routes/analytics/index@analytics.svelte
+++ src/routes/analytics/index@analytics.svelte
@@ -1,13 +1,17 @@
 <script>
+  import DeviceIcon from '$lib/assets/DeviceIcon.svelte';
+  import LocationIcon from '$lib/assets/LocationIcon.svelte';
+  import SourceIcon from '$lib/assets/SourceIcon.svelte';
+
   import Clicks from '$lib/components/analytics/Clicks.svelte';
   import DataPoint from '$lib/components/analytics/DataPoint.svelte';
 </script>
 
 <div class="highlights flex">
   <Clicks clicks="29" title="Today" />
-  <DataPoint name="Top Location" data="Westeros" />
-  <DataPoint name="Top Source" data="Direct" />
-  <DataPoint name="Top Device" data="Skynet" />
+  <DataPoint name="Top Location" data="Westeros"><LocationIcon slot="icon" /></DataPoint>
+  <DataPoint name="Top Source" data="Direct"><SourceIcon slot="icon" /></DataPoint>
+  <DataPoint name="Top Device" data="Skynet"><DeviceIcon slot="icon" /></DataPoint>
 </div>
 
 <style>
