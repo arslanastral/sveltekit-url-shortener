b9b5e8e441c33ebbf495c56d29c556675666a615
Arslan <arslanastral@outlook.com>
2022-04-27 23:15:43 +0800

    added source icon

---
+++ src/lib/assets/SourceIcon.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let width = '40';
+  export let height = '40';
+  export let fill = 'currentColor';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {width} {height} viewBox="0 0 48 48"
+  ><path
+    fill="none"
+    stroke={fill}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="m15 30-9-7L43 7l-9 34-16-10L43 7Z"
+  /><path
+    fill="none"
+    stroke={fill}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="m15 30 1 9 2-8"
+  /></svg
+>
