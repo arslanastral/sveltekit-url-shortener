99109af3b447894b65f593beb907ec1e93f874e3
Arslan <arslanastral@outlook.com>
2022-04-27 22:58:29 +0800

    added top data point component

---
+++ src/lib/components/analytics/DataPoint.svelte
@@ -0,0 +1,45 @@
+<script>
+  export let name = 'Data Name';
+  export let data = 'No Data';
+</script>
+
+<div class="container flex">
+  <span class="icon"><slot name="icon" /></span>
+  <div class="data">{data}</div>
+  <div class="name">{name}</div>
+</div>
+
+<style>
+  .container {
+    width: 206px;
+    height: 120px;
+    background-color: white;
+    border: 1px solid #cdcdcd;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    border-radius: 14px;
+  }
+
+  .icon {
+    margin-left: 12px;
+    width: 100%;
+  }
+
+  .data {
+    margin-left: 12px;
+    width: 100%;
+    font-size: 24px;
+    letter-spacing: -0.045em;
+    color: #6d6d6d;
+  }
+
+  .name {
+    margin-left: 12px;
+    width: 100%;
+    font-weight: 600;
+    font-size: 21px;
+    letter-spacing: -0.045em;
+    color: #000000;
+  }
+</style>
