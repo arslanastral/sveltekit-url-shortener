b324d06e9cebdad5f91537a0cb15dd41fb169146
Arslan <arslanastral@outlook.com>
2022-04-27 21:37:41 +0800

    added analytics __layout

---
+++ src/routes/analytics/__layout-analytics.svelte
@@ -0,0 +1,74 @@
+<script context="module">
+  export async function load({ session }) {
+    if (!session.user) {
+      return {
+        status: 302,
+        redirect: '/login'
+      };
+    }
+    return {};
+  }
+</script>
+
+<script>
+  import PageNavigation from '$lib/components/PageNavigation.svelte';
+
+  let pages = [
+    {
+      name: 'Today',
+      path: '/analytics'
+    },
+    {
+      name: 'This Week',
+      path: '/analytics/weekly'
+    },
+    {
+      name: 'All Time',
+      path: '/analytics/all'
+    }
+  ];
+</script>
+
+<div class="fadeIn wrapper">
+  <div class="flex container">
+    <div class="main-title">Analytics</div>
+    <PageNavigation {pages} />
+    <div class="analytics-container flex">
+      <slot />
+    </div>
+  </div>
+</div>
+
+<style>
+  .wrapper {
+    background-repeat: repeat;
+    background-color: #fafafa;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    min-height: 90vh;
+  }
+
+  .container {
+    width: 80%;
+    margin-top: 20px;
+    flex-direction: column;
+  }
+
+  .main-title {
+    width: 100%;
+    font-size: 35px;
+    font-weight: bold;
+    margin: 10px 0;
+  }
+
+  .analytics-container {
+    width: 100%;
+    margin-top: 30px;
+    background-color: white;
+    box-shadow: 0px 0px 4px -1px rgba(0, 0, 0, 0.25);
+    border-radius: 24px;
+    overflow: hidden;
+    align-items: flex-start;
+  }
+</style>
