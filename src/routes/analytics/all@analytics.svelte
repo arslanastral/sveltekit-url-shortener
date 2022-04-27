b9c30f0a1e237722ce05ff7729bfc831a0e9bf11
Arslan <arslanastral@outlook.com>
2022-04-27 22:37:32 +0800

    style tweaks

--- src/routes/analytics/all@analytics.svelte
+++ src/routes/analytics/all@analytics.svelte
@@ -2,4 +2,4 @@
   import Clicks from '$lib/components/analytics/Clicks.svelte';
 </script>
 
-<Clicks clicks="8K" title="Clicks All Time" />
+<Clicks clicks="8K" title="All Time" />
--- src/routes/analytics/index@analytics.svelte
+++ src/routes/analytics/index@analytics.svelte
@@ -2,4 +2,4 @@
   import Clicks from '$lib/components/analytics/Clicks.svelte';
 </script>
 
-<Clicks clicks="29" title="Clicks Today" />
+<Clicks clicks="29" title="Today" />
