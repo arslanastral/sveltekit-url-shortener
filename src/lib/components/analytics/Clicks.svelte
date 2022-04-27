cea3928705d0a36b7259ad01dc8f626f0c3ba887
Arslan <arslanastral@outlook.com>
2022-04-27 22:11:52 +0800

    added clicks component

---
+++ src/lib/components/analytics/Clicks.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let clicks = '0';
+  export let title = 'Clicks Today';
+</script>
+
+<div class="container flex">
+  <div class="count">{clicks}</div>
+  <div class="title">{title}</div>
+</div>
+
+<style>
+  .container {
+    width: 120px;
+    height: 120px;
+    background-color: #00bf41;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+  }
+
+  .count {
+    width: 100%;
+    color: #ffd977;
+    font-size: 52px;
+  }
+</style>
