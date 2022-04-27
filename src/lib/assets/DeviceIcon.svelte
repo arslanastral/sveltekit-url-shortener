a63ed86fec3a09c7fa992f7bd45ece8dd087d336
Arslan <arslanastral@outlook.com>
2022-04-27 23:19:50 +0800

    added device icon

---
+++ src/lib/assets/DeviceIcon.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let width = '40';
+  export let height = '40';
+  export let fill = 'currentColor';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" {width} {height} viewBox="0 0 48 48"
+  ><path
+    fill="none"
+    stroke={fill}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M29.3 34h12.5c1 0 1.7-.6 1.7-1.3v-1.2h-2.3v-23a1.7 1.7 0 0 0-1.7-1.7h-31a1.7 1.7 0 0 0-1.7 1.7h0v23H4.5v1.3c0 .7.8 1.2 1.7 1.2h5.6"
+  /><path
+    fill="none"
+    stroke={fill}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M13.6 11.2a1.8 1.8 0 0 0-1.8 1.8v26.6a1.7 1.7 0 0 0 1.7 1.6h14.2a1.7 1.7 0 0 0 1.6-1.6V12.9a1.7 1.7 0 0 0-1.7-1.7Z"
+  /><circle cx="16.8" cy="14.6" r=".7" {fill} /><path
+    fill="none"
+    stroke={fill}
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    d="M6.8 31.5h5m29.4 0H29.3M19 14.6h6.2"
+  /></svg
+>
