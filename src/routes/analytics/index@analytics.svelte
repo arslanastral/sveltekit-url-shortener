04a4514ee4ce06be3d40faaa9d81a489d03b781b
Arslan <arslanastral@outlook.com>
2022-04-27 23:05:57 +0800

    added top datapoint to analytics

--- src/routes/analytics/index@analytics.svelte
+++ src/routes/analytics/index@analytics.svelte
@@ -1,5 +1,17 @@
 <script>
   import Clicks from '$lib/components/analytics/Clicks.svelte';
+  import DataPoint from '$lib/components/analytics/DataPoint.svelte';
 </script>
 
-<Clicks clicks="29" title="Today" />
+<div class="highlights flex">
+  <Clicks clicks="29" title="Today" />
+  <DataPoint name="Top Location" data="Westeros" />
+  <DataPoint name="Top Source" data="Direct" />
+  <DataPoint name="Top Device" data="Skynet" />
+</div>
+
+<style>
+  .highlights {
+    gap: 20px;
+  }
+</style>
