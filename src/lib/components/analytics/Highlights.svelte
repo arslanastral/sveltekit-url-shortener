81e0ceb4e8165a23d9dea1b4e6007f275b5dda12
Arslan <arslanastral@outlook.com>
2022-04-28 17:25:22 +0800

    added data points to highlights component

---
+++ src/lib/components/analytics/Highlights.svelte
@@ -0,0 +1,26 @@
+<script>
+  import DeviceIcon from '$lib/assets/DeviceIcon.svelte';
+  import LocationIcon from '$lib/assets/LocationIcon.svelte';
+  import SourceIcon from '$lib/assets/SourceIcon.svelte';
+  import Clicks from '$lib/components/analytics/Clicks.svelte';
+  import DataPoint from '$lib/components/analytics/DataPoint.svelte';
+
+  export let clickCount = '0';
+  export let clickTitle = 'Today';
+  export let topLocation = 'No Data';
+  export let topSource = 'No Data';
+  export let topDevice = 'No Data';
+</script>
+
+<div class="highlights flex">
+  <Clicks clicks={clickCount} title={clickTitle} />
+  <DataPoint name="Top Location" data={topLocation}><LocationIcon slot="icon" /></DataPoint>
+  <DataPoint name="Top Source" data={topSource}><SourceIcon slot="icon" /></DataPoint>
+  <DataPoint name="Top Device" data={topDevice}><DeviceIcon slot="icon" /></DataPoint>
+</div>
+
+<style>
+  .highlights {
+    gap: 20px;
+  }
+</style>
