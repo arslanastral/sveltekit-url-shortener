59cd9d587f59640d387c7b972fdcdead3ce41515
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-28 17:25:22 +0800

    added data points to highlights component

--- src/routes/analytics/index@analytics.svelte
+++ src/routes/analytics/index@analytics.svelte
@@ -1,21 +1,13 @@
 <script>
-  import DeviceIcon from '$lib/assets/DeviceIcon.svelte';
-  import LocationIcon from '$lib/assets/LocationIcon.svelte';
-  import SourceIcon from '$lib/assets/SourceIcon.svelte';
+  import Highlights from '$lib/components/analytics/Highlights.svelte';
 
-  import Clicks from '$lib/components/analytics/Clicks.svelte';
-  import DataPoint from '$lib/components/analytics/DataPoint.svelte';
+  let data = {
+    clickCount: '0',
+    clickTitle: 'Today',
+    topLocation: 'No Data',
+    topSource: 'No Data',
+    topDevice: 'No Data'
+  };
 </script>
 
-<div class="highlights flex">
-  <Clicks clicks="29" title="Today" />
-  <DataPoint name="Top Location" data="Westeros"><LocationIcon slot="icon" /></DataPoint>
-  <DataPoint name="Top Source" data="Direct"><SourceIcon slot="icon" /></DataPoint>
-  <DataPoint name="Top Device" data="Skynet"><DeviceIcon slot="icon" /></DataPoint>
-</div>
-
-<style>
-  .highlights {
-    gap: 20px;
-  }
-</style>
+<Highlights {...data} />
