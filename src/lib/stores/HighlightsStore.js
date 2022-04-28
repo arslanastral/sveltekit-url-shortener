d9d316c84890e40dca99066073a08c1e60afb4b5
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-28 17:49:25 +0800

    refactor highlights

--- src/lib/stores/HighlightsStore.js
+++ src/lib/stores/HighlightsStore.js
@@ -1,8 +1,24 @@
 import { writable } from 'svelte/store';
 
-export const Clicks = writable({ clickCount: '0', clickTitle: 'Today' });
+export const TodayData = writable({
+  clickCount: '0',
+  clickTitle: 'Today',
+  topLocation: 'No Data',
+  topSource: 'No Data',
+  topDevice: 'No Data'
+});
 
-export const TopData = writable({
+export const WeeklyData = writable({
+  clickCount: '0',
+  clickTitle: 'This Week',
+  topLocation: 'No Data',
+  topSource: 'No Data',
+  topDevice: 'No Data'
+});
+
+export const AllData = writable({
+  clickCount: '0',
+  clickTitle: 'All Time',
   topLocation: 'No Data',
   topSource: 'No Data',
   topDevice: 'No Data'
