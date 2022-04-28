4bede4ac6a87c97793291804d232f3839b4b0da0
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-28 17:28:40 +0800

    added highlights to __layout

--- src/routes/analytics/all@analytics.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  import Clicks from '$lib/components/analytics/Clicks.svelte';
-</script>
-
-<Clicks clicks="8K" title="All Time" />
--- src/routes/analytics/weekly@analytics.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-  import Clicks from '$lib/components/analytics/Clicks.svelte';
-</script>
-
-<Clicks clicks="243" title="This Week" />
