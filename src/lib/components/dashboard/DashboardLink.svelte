05553340c74093381f8268de60f84881dc188682
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-30 23:32:04 +0800

    moved link code to seperate component

--- src/lib/components/dashboard/DashboardLink.svelte
+++ src/lib/components/dashboard/DashboardLink.svelte
@@ -1,13 +1,11 @@
 <script>
-  import { browser } from '$app/env';
-  import LockIcon from '$lib/assets/LockIcon.svelte';
   import { timeAgo } from '$lib/utils/timeAgo';
   import LinkButton from '../recent/LinkButton.svelte';
   import QrCode from '$lib/components/shortener/QRCode.svelte';
   import Edit from './Edit.svelte';
-  import Tag from './Tag.svelte';
   import { page } from '$app/stores';
   import ClicksIcon from '$lib/assets/ClicksIcon.svelte';
+  import Link from '../Link.svelte';
 
   export let index;
   export let short_url;
@@ -41,26 +39,7 @@
 
 <div class="flex link-container">
   <span class="index">{index}</span>
-  <div class="link">
-    <div class="long-link">
-      <img src={`https://www.google.com/s2/favicons?domain=${long_url}`} alt="favicon" />{long_url}
-    </div>
-
-    <div class="flex">
-      <a rel="external" href={shortLink} target="_blank" class="short-link">{shortLink}</a>
-      {#if secured}
-        <LockIcon width="18" height="18" fill="green" />
-      {/if}
-    </div>
-
-    {#if tags.length}
-      <div class="flex tags">
-        {#each tags as tag}
-          <Tag tag={tag.name} --bg-color={tag.color} --color={'black'} />
-        {/each}
-      </div>
-    {/if}
-  </div>
+  <Link {shortLink} {long_url} {secured} {tags} />
   <div class="date">{timeAgo(created_at)}</div>
   <div class="clicks flex"><ClicksIcon /><span>{clicks}</span></div>
   <div class="button-container flex">
@@ -88,42 +67,6 @@
     height: 20px;
     font-weight: 500;
     font-size: 19px;
-  }
-
-  .link {
-    margin: 20px 20px 20px 0;
-    display: flex;
-    flex-direction: column;
-  }
-
-  img {
-    margin-right: 10px;
-  }
-
-  .long-link {
-    font-size: 17px;
-    color: #636363;
-    display: inline-block;
-    width: clamp(200px, 40vw, 900px);
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-  }
-
-  .short-link {
-    margin-right: 6px;
-    text-decoration: none;
-    color: black;
-    font-weight: 500;
-    font-size: 19px;
-  }
-
-  .short-link:hover {
-    text-decoration: underline;
-  }
-
-  .tags {
-    flex-wrap: wrap;
   }
 
   .date {
