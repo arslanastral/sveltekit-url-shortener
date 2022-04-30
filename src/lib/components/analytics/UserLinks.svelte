7369630b22a3107aa9c79d60f39f73bf72258871
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-04-30 22:31:38 +0800

    created user links component

---
+++ src/lib/components/analytics/UserLinks.svelte
@@ -0,0 +1,11 @@
+<div class="links-container" />
+
+<style>
+  .links-container {
+    background: #ffffff;
+    box-shadow: 0px 0px 4px -1px rgba(0, 0, 0, 0.25);
+    border-radius: 24px;
+    min-height: 500px;
+    width: 40%;
+  }
+</style>
