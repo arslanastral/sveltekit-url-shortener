e26565666f872d3e79fd98d21f6a3a3d035be4bf
Arslan <arslanastral@outlook.com>
2022-05-02 13:26:22 +0800

    created chart component

---
+++ src/lib/components/analytics/Chart.svelte
@@ -0,0 +1,11 @@
+<div class="chart-container" />
+
+<style>
+  .chart-container {
+    background: #ffffff;
+    box-shadow: 0px 0px 4px -1px rgba(0, 0, 0, 0.25);
+    border-radius: 24px;
+    min-height: 550px;
+    width: 60%;
+  }
+</style>
