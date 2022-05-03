925ada387baabf1d9159baf0513a69e893c0e78d
Arslan <arslanastral@outlook.com>
2022-05-03 23:54:38 +0800

    collected user device type and browser info

--- src/lib/services/collectAnalytics.js
+++ src/lib/services/collectAnalytics.js
@@ -1,11 +1,14 @@
 import { useCollection } from '$lib/utils/useCollection';
+import parser from 'ua-parser-js';
 
 export async function collectAnalytics(shortUrl, user, userAgent, location, referer) {
   const analytics = await useCollection('analytics');
 
+  const { browser, os, device } = parser(userAgent);
+
   await analytics.insertOne({
     metadata: { short_url: shortUrl, created_by: user },
-    ua: userAgent,
+    info: { browser: browser.name, os: os.name, device: device.type },
     location: location,
     ref: referer,
     timestamp: new Date(),
