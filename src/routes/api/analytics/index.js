7918465df9d5da871cb65b5894c968d7811c81d3
Arslan <arslanastral@outlook.com>
2022-05-06 20:18:38 +0800

    used aggregation to query analytics data

--- src/routes/api/analytics/index.js
+++ src/routes/api/analytics/index.js
@@ -13,16 +13,21 @@ export async function get({ locals }) {
   try {
     const collection = await useCollection('analytics');
 
-    const links = await collection
-      .find({
-        'metadata.created_by': currentUser.email,
-        timestamp: { $gte: new Date(new Date().setHours(0, 0, 0, 0)) }
-      })
-      .sort({ _id: -1 })
-      .project({
-        _id: 0
-      })
-      .toArray();
+    let analyticsPipeline = [
+      {
+        $match: {
+          'metadata.created_by': currentUser.email,
+          timestamp: { $gte: new Date(new Date().setHours(0, 0, 0, 0)) }
+        }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      }
+    ];
+
+    const links = await collection.aggregate(analyticsPipeline).toArray();
 
     return {
       status: 200,
