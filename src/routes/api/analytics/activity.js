21f7072c9ee8ad7f7c531a4c57d7f5edc45b3b90
Arslan <arslanastral@outlook.com>
2022-05-20 14:02:22 +0800

    implemented activity endpoint

--- src/routes/api/analytics/activity.js
+++ src/routes/api/analytics/activity.js
@@ -9,4 +9,56 @@ export async function get({ locals }) {
       body: { error: 'Unauthorized' }
     };
   }
+
+  try {
+    const collection = await useCollection('analytics');
+
+    // if (id) {
+    //   let link = await collection.findOne(linkQuery);
+
+    //   if (!link) {
+    //     return {
+    //       status: 404,
+    //       body: { error: 'Link not found' }
+    //     };
+    //   }
+    // }
+
+    let analyticsPipeline = [
+      {
+        $match: {
+          'metadata.created_by': currentUser.email
+          //   ...(id && linkQuery),
+          //   ...(time !== 'all' && timeQuery)
+        }
+      },
+
+      {
+        $group: {
+          _id: { day: { $dayOfMonth: '$timestamp' }, hour: { $hour: '$timestamp' } },
+          count: { $sum: 1 }
+        }
+      },
+      {
+        $project: {
+          _id: 0,
+          count: '$count',
+          day: '$_id.day',
+          hour: '$_id.hour'
+        }
+      }
+    ];
+
+    const links = await collection.aggregate(analyticsPipeline).toArray();
+
+    return {
+      status: 200,
+      body: links
+    };
+  } catch (error) {
+    return {
+      status: 500,
+      body: { error: 'Internal Server Error' }
+    };
+  }
 }
