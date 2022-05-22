40773f7feca0d7512d0407d9a15ac26db495b932
Arslan <arslanastral@outlook.com>
2022-05-22 19:56:05 +0800

    added check for data

--- src/lib/components/analytics/Chart.svelte
+++ src/lib/components/analytics/Chart.svelte
@@ -2,12 +2,7 @@
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
 
-  let data = [
-    { count: 10, hour: 6 },
-    { count: 5, hour: 5 },
-    { count: 4, hour: 4 },
-    { count: 2, hour: 1 }
-  ];
+  let data = [{ count: 12, hour: 7 }];
 
   let chart;
   const dimensions = {
@@ -16,85 +11,87 @@
   };
 
   onMount(() => {
-    const svg = d3.select(chart);
+    if (data.length) {
+      const svg = d3.select(chart);
 
-    const xScale = d3
-      .scaleBand()
-      .domain([
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
-      ])
-      .range([0, dimensions.width])
-      .padding(0.4);
+      const xScale = d3
+        .scaleBand()
+        .domain([
+          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
+        ])
+        .range([0, dimensions.width])
+        .padding(0.4);
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([d3.min(data.map((d) => d.count)), d3.max(data.map((d) => d.count))])
-      .range([dimensions.height, 0])
-      .nice();
+      const yScale = d3
+        .scaleLinear()
+        .domain([d3.min(data.map((d) => d.count)), d3.max(data.map((d) => d.count))])
+        .range([dimensions.height, 0])
+        .nice();
 
-    const xAxis = d3
-      .axisBottom(xScale)
+      const xAxis = d3
+        .axisBottom(xScale)
 
-      .tickValues(
-        xScale.domain().filter(function (d, i) {
-          const MIN_WIDTH = 70;
-          let skip = Math.round((MIN_WIDTH * data.length) / dimensions.width);
-          skip = Math.max(2, skip);
-          return !(i % skip);
+        .tickValues(
+          xScale.domain().filter(function (d, i) {
+            const MIN_WIDTH = 70;
+            let skip = Math.round((MIN_WIDTH * data.length) / dimensions.width);
+            skip = Math.max(2, skip);
+            return !(i % skip);
+          })
+        )
+        .tickPadding(20);
+      svg
+        .select('.x-axis')
+        .style('transform', `translate(50px,${dimensions.height}px)`)
+        .attr('font-family', 'Inter')
+        .attr('font-size', '1rem')
+        .attr('color', '#615b5b')
+        .transition()
+        .duration(300)
+        .call(xAxis);
+
+      svg.select('.x-axis').select('path').remove();
+
+      const yAxis = d3
+        .axisLeft(yScale)
+        .ticks(8)
+
+        .tickSize(-dimensions.width - 50);
+      svg
+        .select('.y-axis')
+        .attr('font-family', 'Inter')
+        .attr('font-size', '0.9rem')
+        .attr('color', '#615b5b')
+        .call(yAxis)
+        .call((g) => g.select('.domain').remove())
+        .call((g) =>
+          g
+            .selectAll('.tick:not(:first-of-type) line')
+            .attr('stroke-opacity', 0.5)
+            .attr('stroke-dasharray', '2,2')
+        )
+        .call((g) => g.selectAll('.tick text').attr('x', 35).attr('dy', -7));
+
+      svg
+        .selectAll('.bar')
+        .data(data)
+        .join('rect')
+        .attr('class', 'bar')
+        .style('transform', 'scale(1,-1)')
+        .attr('rx', 1)
+        .attr('x', (value) => xScale(value.hour) + 50)
+        .attr('y', -dimensions.height)
+        .attr('width', xScale.bandwidth())
+        .on('mouseover', function (event, d) {
+          d3.select(this).attr('fill', 'greenyellow');
         })
-      )
-      .tickPadding(20);
-    svg
-      .select('.x-axis')
-      .style('transform', `translate(50px,${dimensions.height}px)`)
-      .attr('font-family', 'Inter')
-      .attr('font-size', '1rem')
-      .attr('color', '#615b5b')
-      .transition()
-      .duration(300)
-      .call(xAxis);
-
-    svg.select('.x-axis').select('path').remove();
-
-    const yAxis = d3
-      .axisLeft(yScale)
-      .ticks(8)
-
-      .tickSize(-dimensions.width - 50);
-    svg
-      .select('.y-axis')
-      .attr('font-family', 'Inter')
-      .attr('font-size', '0.9rem')
-      .attr('color', '#615b5b')
-      .call(yAxis)
-      .call((g) => g.select('.domain').remove())
-      .call((g) =>
-        g
-          .selectAll('.tick:not(:first-of-type) line')
-          .attr('stroke-opacity', 0.5)
-          .attr('stroke-dasharray', '2,2')
-      )
-      .call((g) => g.selectAll('.tick text').attr('x', 35).attr('dy', -7));
-
-    svg
-      .selectAll('.bar')
-      .data(data)
-      .join('rect')
-      .attr('class', 'bar')
-      .style('transform', 'scale(1,-1)')
-      .attr('rx', 1)
-      .attr('x', (value) => xScale(value.hour) + 50)
-      .attr('y', -dimensions.height)
-      .attr('width', xScale.bandwidth())
-      .on('mouseover', function (event, d) {
-        d3.select(this).attr('fill', 'greenyellow');
-      })
-      .on('mouseout', function () {
-        d3.select(this).attr('fill', 'blue');
-      })
-      .transition()
-      .attr('height', (value) => dimensions.height - yScale(value.count))
-      .attr('fill', 'blue');
+        .on('mouseout', function () {
+          d3.select(this).attr('fill', 'blue');
+        })
+        .transition()
+        .attr('height', (value) => dimensions.height - yScale(value.count))
+        .attr('fill', 'blue');
+    }
   });
 </script>
 
