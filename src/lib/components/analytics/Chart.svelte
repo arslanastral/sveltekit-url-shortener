0d7c50ab89bbbf49a090785568872d6b7e470a6b
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-06-01 19:28:10 +0800

    used d3.range

--- src/lib/components/analytics/Chart.svelte
+++ src/lib/components/analytics/Chart.svelte
@@ -16,9 +16,7 @@
 
       const xScale = d3
         .scaleBand()
-        .domain([
-          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
-        ])
+        .domain(d3.range(1, 24))
         .range([0, dimensions.width])
         .padding(0.4);
 
