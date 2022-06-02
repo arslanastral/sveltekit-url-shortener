7e64d354ed62bc8fbb23f5faa42be5cc40cc3d91
Arslan <arslanastral@outlook.com>
2022-06-02 16:16:40 +0800

    removed auto import

--- src/lib/components/analytics/Chart.svelte
+++ src/lib/components/analytics/Chart.svelte
@@ -1,7 +1,6 @@
 <script>
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
-  import { each } from 'svelte/internal';
 
   export let data = [];
 
@@ -30,7 +29,6 @@
 
       const xAxis = d3
         .axisBottom(xScale)
-
         .tickValues(
           xScale.domain().filter(function (d, i) {
             const MIN_WIDTH = 70;
