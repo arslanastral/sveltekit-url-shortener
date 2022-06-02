4e1b008f8373e172455a913eeff6c569ce4b201f
Arslan <arslanastral@outlook.com>
2022-06-02 19:14:39 +0800

    style: overflow-y

--- src/lib/components/analytics/UserLinks.svelte
+++ src/lib/components/analytics/UserLinks.svelte
@@ -34,7 +34,8 @@
     background: #ffffff;
     box-shadow: 0px 0px 4px -1px rgba(0, 0, 0, 0.25);
     border-radius: 24px;
-    min-height: 550px;
+    height: 550px;
+    overflow-y: auto;
     width: 40%;
   }
 
