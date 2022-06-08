921bb561c392704bf9a1587838c219d2e71120e7
Arslan <arslanastral@outlook.com>
2022-06-08 23:07:54 +0800

    perf (QR generation): added new library

--- src/lib/utils/generateQRCode.js
+++ src/lib/utils/generateQRCode.js
@@ -1,9 +1,14 @@
-export async function generateQRCode(text, size = 230) {
-  const { default: kjua } = await import('kjua');
-  return kjua({
-    text,
-    fill: 'blue',
-    rounded: 100,
-    size
-  });
+export async function generateQRCode(text, node) {
+  const { default: QrCreator } = await import('qr-creator');
+  return QrCreator.render(
+    {
+      text: text,
+      radius: 0.5, // 0.0 to 0.5
+      ecLevel: 'H', // L, M, Q, H
+      fill: 'blue', // foreground color
+      background: null, // color or null for transparent
+      size: 250 // in pixels
+    },
+    node
+  );
 }
