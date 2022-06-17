aa50dea35184a7d819365ddb0b602d8c611f3bd9
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-06-17 14:53:28 +0800

    fix(highlights): time query string

--- src/routes/analytics/all@analytics.svelte
+++ src/routes/analytics/all@analytics.svelte
@@ -7,7 +7,7 @@
       };
     }
 
-    const res = await fetch('/api/analytics/highlights?time=week');
+    const res = await fetch('/api/analytics/highlights?time=all');
     const activity = await fetch('/api/analytics/activity?time=weekly');
 
     if (res.ok && activity.ok) {
