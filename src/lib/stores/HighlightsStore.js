a67d6257db4f23ac388719cd011bd958d97f4ca7
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-06-17 14:51:12 +0800

    fix(highlights): changed default state

--- src/lib/stores/HighlightsStore.js
+++ src/lib/stores/HighlightsStore.js
@@ -1,25 +1,7 @@
 import { writable } from 'svelte/store';
 
-export const TodayData = writable({
-  clickCount: '0',
-  clickTitle: 'Today',
-  topLocation: 'No Data',
-  topSource: 'No Data',
-  topDevice: 'No Data'
-});
+export const TodayData = writable({});
 
-export const WeeklyData = writable({
-  clickCount: '0',
-  clickTitle: 'This Week',
-  topLocation: 'No Data',
-  topSource: 'No Data',
-  topDevice: 'No Data'
-});
+export const WeeklyData = writable({});
 
-export const AllData = writable({
-  clickCount: '0',
-  clickTitle: 'All Time',
-  topLocation: 'No Data',
-  topSource: 'No Data',
-  topDevice: 'No Data'
-});
+export const AllData = writable({});
