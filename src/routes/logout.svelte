613f1d4b4a76400f7aa685901797d47d36c9c067
Arslan <arslanastral@outlook.com>
2022-06-19 20:00:34 +0800

    fix(recents): remove recents on logout

--- src/routes/logout.svelte
+++ src/routes/logout.svelte
@@ -1,14 +1,17 @@
 <script context="module">
   import { browser } from '$app/env';
   import { session } from '$app/stores';
+  import { UserRecentStore } from '$lib/stores/RecentStore';
 
   export async function load({ fetch }) {
     await fetch('/auth/logout', {
       method: 'POST'
     });
 
-    if (browser) session.set({ user: null });
-
+    if (browser) {
+      session.set({ user: null });
+      UserRecentStore.set([]);
+    }
     return {
       status: 302,
       redirect: '/'
