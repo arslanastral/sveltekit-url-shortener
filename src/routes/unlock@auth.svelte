d247d2fd58784ef94712f858e8e880cc55e2f3f8
Arslan <arslanastral@outlook.com>
2022-06-22 01:41:12 +0800

    style(unlock): added layout

---
+++ src/routes/unlock@auth.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  export async function load({ url }) {
+    const id = url.searchParams.get('id');
+
+    if (!id || id.length > 4) {
+      return {
+        status: 302,
+        redirect: '/'
+      };
+    }
+
+    return {
+      props: {
+        id
+      }
+    };
+  }
+</script>
+
+<script>
+  import Unlock from '$lib/components/unlock/Unlock.svelte';
+
+  export let id;
+</script>
+
+<Unlock {id} />
