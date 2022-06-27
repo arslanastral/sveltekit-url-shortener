a7db163c008a27b71c35d866f819fbfbcc57db8e
Arslan <arslanastral@outlook.com>
2022-06-27 15:32:28 +0800

    feat(nav): adding error pages

---
+++ src/routes/analytics/__error.svelte
@@ -0,0 +1,14 @@
+<h1>Oops! There was an error fetching analytics</h1>
+<p>Try turning off your adblocker or refresh the page.</p>
+
+<style>
+  h1 {
+    margin: 0;
+  }
+
+  p {
+    color: grey;
+    font-size: 18px;
+    margin-top: 5px;
+  }
+</style>
