aa4ddcb89d66995b2990202b35dfe2b215c25e0a
Arslan <arslanastral@outlook.com>
2022-06-27 22:20:49 +0800

    created auth button component

---
+++ src/lib/components/login/AuthButton.svelte
@@ -0,0 +1,34 @@
+<script>
+  import ButtonLoader from '$lib/assets/ButtonLoader.svelte';
+
+  export let name;
+  export let isProcessing = false;
+  export let onClick = undefined;
+</script>
+
+<button on:click={onClick} class="auth-button flex">
+  {#if isProcessing}
+    <ButtonLoader width="100%" height="100%" />
+  {:else}
+    <span class="fadeIn">{name}</span>
+  {/if}</button
+>
+
+<style>
+  .auth-button {
+    font-size: 18px;
+    width: 100%;
+    margin-top: 20px;
+    justify-content: center;
+    border: none;
+    background-color: #3e5dff;
+    color: white;
+    height: 40px;
+    border-radius: 6px;
+    transition: transform ease-out 0.09s;
+  }
+
+  .auth-button:active {
+    transform: scale(0.97);
+  }
+</style>
