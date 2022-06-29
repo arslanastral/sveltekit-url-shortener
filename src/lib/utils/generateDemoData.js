5ad40090d0cf589d6d9c2816d8920c930ba95d00
Arslan <arslanastral@outlook.com>
2022-06-29 17:30:09 +0800

    created demo data generation util

---
+++ src/lib/utils/generateDemoData.js
@@ -0,0 +1,22 @@
+import { timeDay, timeHour } from 'd3';
+
+export function generateDemoData(id) {
+  let week = timeDay.range(timeDay.offset(new Date(), -7), new Date(), 1);
+  let hours = timeHour.range(timeDay.offset(new Date(), -1), new Date(), 1);
+
+  let dates = week.concat(hours);
+
+  let data = dates.map((e) => {
+    let obj = {
+      metadata: { short_url: id, created_by: 'demo@ky.com' },
+      info: { browser: 'Chrome', os: 'Windows', device: null },
+      location: 'Saturn',
+      ref: null,
+      timestamp: e,
+      clicks: 1
+    };
+    return obj;
+  });
+
+  return data;
+}
