77c4342c3475edbda4d32ec91e3f14760c7e2837
Arslan <arslanastral@outlook.com>
2022-07-01 17:25:11 +0800

    refactor(demo): moved generator

--- src/lib/utils/insertDemoData.js
+++ src/lib/utils/insertDemoData.js
@@ -1,5 +1,5 @@
 import { useCollection } from '$lib/utils/useCollection';
-import { generateDemoData } from './generateDemoData';
+import { utcDay, utcHour } from 'd3';
 import { nanoid } from 'nanoid';
 
 export async function insertDemoData() {
@@ -31,4 +31,29 @@ export async function insertDemoData() {
   } catch (error) {
     console.log(error);
   }
+}
+
+function generateDemoData(id) {
+  let currentDate = new Date();
+  let flooredDate = utcDay.floor(currentDate);
+  let lastWeek = utcDay.offset(flooredDate, -7);
+
+  let week = utcDay.range(lastWeek, flooredDate, 1);
+  let hours = utcHour.range(flooredDate, currentDate, 1);
+
+  let dates = week.concat(hours);
+
+  let data = dates.map((e) => {
+    let obj = {
+      metadata: { short_url: id, created_by: 'demo@ky.com' },
+      info: { browser: 'Chrome', os: 'Windows', device: null },
+      location: 'Saturn',
+      ref: null,
+      timestamp: e,
+      clicks: 1
+    };
+    return obj;
+  });
+
+  return data;
 }
