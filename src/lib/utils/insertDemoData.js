4195f2285671a127fa88b3f9010bc35daf70eeaa
Arslan <arslanastral@outlook.com>
2022-07-02 16:10:44 +0800

    feat(demo): random clicks

--- src/lib/utils/insertDemoData.js
+++ src/lib/utils/insertDemoData.js
@@ -38,12 +38,25 @@ function generateDemoData(id) {
   let flooredDate = utcDay.floor(currentDate);
   let lastWeek = utcDay.offset(flooredDate, -7);
 
-  let week = utcDay.range(lastWeek, flooredDate, 1);
+  let week = utcDay.range(lastWeek, currentDate, 1);
   let hours = utcHour.range(flooredDate, currentDate, 1);
 
   let dates = week.concat(hours);
 
-  let data = dates.map((e) => {
+  let randomClicks = dates
+    .map((e) => {
+      let rand = Math.floor(Math.random() * (40 - 8 + 1)) + 8;
+      let duplicates = [];
+
+      for (let i = 0; i < rand; i++) {
+        duplicates.push(e);
+      }
+
+      return duplicates;
+    })
+    .flat();
+
+  let data = randomClicks.map((e) => {
     let obj = {
       metadata: { short_url: id, created_by: 'demo@ky.com' },
       info: { browser: 'Chrome', os: 'Windows', device: null },
