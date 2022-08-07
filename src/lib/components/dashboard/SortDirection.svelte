ff2b87647fb28c59e61f7b2876b01567344e6f21
Arslan <81504779+arslanastral@users.noreply.github.com>
2022-08-07 18:14:19 +0800

    style(sort): spacing

--- src/lib/components/dashboard/SortDirection.svelte
+++ src/lib/components/dashboard/SortDirection.svelte
@@ -3,7 +3,7 @@
   export let sortDirection;
 </script>
 
-<div class="direction">
+<div class="flex direction">
   <button on:click={() => setSortDirection('asc')} class:direction-active={sortDirection === 'asc'}
     >↑</button
   >
@@ -14,29 +14,22 @@
 </div>
 
 <style>
+  .direction {
+    gap: 8px;
+  }
+
   button {
     border: none;
     cursor: pointer;
-  }
-
-  .direction {
-    display: flex;
-    color: white;
-  }
-
-  .direction button {
     width: 30px;
     height: 30px;
     border-radius: 50%;
-  }
-
-  .direction button:hover {
-    color: yellow;
-    background-color: rgb(85, 85, 85);
+    background: rgb(238, 238, 238);
+    color: blue;
   }
 
   .direction-active {
-    color: yellow;
-    background-color: rgb(85, 85, 85);
+    color: white;
+    background-color: blue;
   }
 </style>
