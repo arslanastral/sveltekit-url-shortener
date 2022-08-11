7716ebb900da107d8339c0b078795d9fe2ea9ac3
Arslan <arslanastral@outlook.com>
2022-08-11 22:06:27 +0800

    added filter menu

--- src/lib/components/dashboard/Filter.svelte
+++ src/lib/components/dashboard/Filter.svelte
@@ -1,10 +1,28 @@
 <script>
   export let isToggled = false;
+
+  const toggle = () => {
+    isToggled = !isToggled;
+  };
 </script>
 
-<button class="control-button">Filter By</button>
+<div class="container">
+  <button on:click={toggle} class="control-button">Filter</button>
+
+  {#if isToggled}
+    <div class="flex menu fadeIn" />
+  {/if}
+</div>
 
 <style>
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+  }
+
   .control-button {
     border: none;
     color: black;
@@ -12,5 +30,19 @@
     border-radius: 10px;
     padding: 8px 15px;
     cursor: pointer;
+    position: relative;
+  }
+
+  .menu {
+    gap: 10px;
+    position: absolute;
+    min-width: 150px;
+    top: 50px;
+    right: 0px;
+    background: white;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 10px;
+    z-index: 3;
   }
 </style>
